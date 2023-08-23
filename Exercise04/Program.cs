2d89577b1c8347d980e9b434144da86936e0e9dc
Urrea <Surrea1411@gmail.com>
2023-08-22 21:01:05 -0500

    Exercise 04

---
+++ Exercise04/Program.cs
@@ -0,0 +1,49 @@
+﻿Console.WriteLine($"Hello");
+Console.WriteLine($"In this space you will know if a year is a leap year or not");
+do
+{
+    Console.WriteLine($"=============");
+    Console.WriteLine($"Please enter the year and press enter");
+    int year = Convert.ToInt32(Console.ReadLine());
+    
+    DateTime dateTime = DateTime.Now;
+    string aux;
+    if (dateTime.Year == year) aux = "is";
+    else if (dateTime.Year > year) aux = "was";
+    else aux = "will";
+    
+    if (IsLeapYear(year))
+    {
+        Console.WriteLine($"The year {year} {aux} a leap year");
+    }
+    else
+    {
+        Console.WriteLine($"The year {year} {aux} not a leap year");
+    }
+} while (true);
+
+static bool IsLeapYear(int year)
+{
+    if (year % 4 == 0)
+    {
+        if (year % 100 == 0)
+        {
+            if (year % 400 == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        else
+        {
+            return true;
+        }
+    }
+    else
+    {
+        return false;
+    }
+}
